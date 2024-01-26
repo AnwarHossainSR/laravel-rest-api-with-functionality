1acb8acc3d6516a4da63ec897aa14ebda3f8fc09
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-01-26 12:29:25 +0600

    Add dependencies and update code

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,43 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Notifications\ResetPasswordNotification;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Traits\Common\AllowCamelCase;
+use App\Traits\Common\CustomJwtToken;
+use App\Traits\PermissionsAndRoles\CustomPermission;
+use App\Traits\Blogs\CustomBlog;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use AllowCamelCase;
+    use CustomJwtToken;
+    use CustomPermission;
+    use CustomBlog;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var string[]
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'contactNo',
+        'profilePhotoPath'
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -36,10 +48,15 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
+
 }
---
+++ resources/lang/en/messages.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to our application',
+    'confirmPassword' => 'Password does not match',
+    'invalidJson' => 'Invalid JSON data given for processing',
+    'notFound' => 'The data with the specified ID cannot be found',
+    'notFoundAccount' => 'The account is not found',
+    'notFoundApprover' => 'The approver is not found',
+    'notAuthorizedApprover' => 'You are not authorized approver!',
+    'username_required' => 'Username is required',
+    'username_string' => 'Please input a valid string',
+    'username_unique' => 'Username is not unique',
+    'password_required' => 'Password is required',
+    'inavlid_email' => 'Email is not valid',
+    'deactivate' => 'Successfully deactivated',
+    'notFoundUrl' => 'Not Found With Given URL',
+    'notFoundId' => 'ID is not found',
+    'queryException' => 'Ops! Internal Server Errors',
+    'notAllowedHTTPMethod' => 'This HTTP method is not allowed. Please select the appropriate option',
+
+    'commonMessageForInputError' => 'The given data was invalid.',
+    'registered' => 'The registration is successfully done!',
+    'registerFailed' => 'Registration failed!',
+    'loggedIn' => 'The user is logged In successfully!',
+    'loginFailed' => 'Not authenticated!',
+    'loggedOut' => 'The user is logged out successfully!',
+    'matchOldPassword' => 'The :attribute is match with current password.',
+    'notMatchCurrentPassword' => 'The :attribute is not match.',
+    'runCommandText' => 'Please run below commands',
+
+    'crud' => [
+        'stored' => 'The data is stored successfully！',
+        'storeFailed' => 'Something wrong! Store failed.',
+        'updated' => 'The data is updated successfully！',
+        'updateFailed' => 'Something wrong! Update failed.',
+        'deleted' => 'The data is deleted successfully！',
+        'deleteFailed' => 'Something wrong! Delete failed.',
+        'notSelfdelete' => 'Sorry! you cannot delete yourself.',
+    ]
+    
+];
