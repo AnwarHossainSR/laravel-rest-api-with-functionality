67d2c7b909169195092407d2103314c846d38ded
Anwar Hossain <mahedianwar@gmail.com>
2024-02-03 12:00:23 +0600

    Add UserResource and User model to PostResource

--- app/Http/Resources/Blogs/PostResource.php
+++ app/Http/Resources/Blogs/PostResource.php
@@ -4,6 +4,9 @@ namespace App\Http\Resources\Blogs;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Carbon;
+use App\Http\Resources\Users\UserResource;
+use App\Models\User;
+
 
 class PostResource extends JsonResource
 {
@@ -25,6 +28,7 @@ class PostResource extends JsonResource
             'imagePath' => $this->image_path,
             'thumbnailPath' => $this->thumbnail_path,
             'comments' => CommentResource::collection($this->comments),
+            'author' => new UserResource(User::find($this->created_by)), // Fetch the user by ID
             'publishedAt' => $this->published_at,
             'createdAt' => Carbon::parse($this->created_at)->format('jS M, Y'),
             'updatedAt' => $this->updated_at,
