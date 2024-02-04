ffdaa8ba8f5cc743f027226e59fa7b870c53b4dc
Anwar Hossain <mahedianwar@gmail.com>
2024-02-04 20:38:13 +0600

    Update UserResource to include photo field

--- app/Http/Resources/Users/UserResource.php
+++ app/Http/Resources/Users/UserResource.php
@@ -16,10 +16,11 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id, 
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'roles' => RoleResource::customCollection($this->roles, ['hidePermission' => true])
+            'photo' => $this->profile_photo_path,
+            //'roles' => RoleResource::customCollection($this->roles, ['hidePermission' => true])
         ];
     }
 }
